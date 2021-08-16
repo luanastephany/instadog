c1323b35bdb1693720447ad994d556a5170a05df
Luana Stephany <ls.luanastephany@gmail.com>
2021-08-16 10:58:03 -0300

    att configs do app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Login from './components/Login/'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div>
-      App React
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
