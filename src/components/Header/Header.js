32358c725a4d4d66721f2d4f64bee9297bf0e4b3
Luana Stephany <ls.luanastephany@gmail.com>
2021-08-17 13:34:29 -0300

    criando e estilizando header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,15 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import './Header.scss'
+import { ReactComponent as Dogs } from '../../Assets/dogs.svg'
 
 const Header = () => {
   return (
-    <div>
-      <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/login'>Login / Criar</Link>
+    <header className="header">
+      <nav className="nav container ">
+        <Link className="logo" to='/'>
+          <Dogs />
+        </Link>
+        <Link className="login" to='/login'>Login / Criar</Link>
       </nav>
-    </div>
+    </header>
   )
 }
 
